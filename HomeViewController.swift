337bbfdb965b7ab4c18b67edce8f3f47530d54c6
Xiguan <zhijiedianshangapple01@zhijiedianshangAPPLE01deMacBook-Pro.local>
2017-12-13 10:07:22 +0800

    创建HomeViewController
    HomeViewController

---
+++ HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  ZGLWeibo_Swift
+//
+//  Created by 智捷电商APPLE01 on 2017/12/13.
+//  Copyright © 2017年 智捷电商APPLE01. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
