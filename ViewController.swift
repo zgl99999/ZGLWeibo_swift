1f9ffa50211ebb611105f38797d81c7486adcd35
Xiguan <zhijiedianshangapple01@zhijiedianshangAPPLE01deMacBook-Pro.local>
2017-12-13 10:02:50 +0800

    Initial commit
    first  connection
    hello world!

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ZGLWeibo_Swift
+//
+//  Created by 智捷电商APPLE01 on 2017/12/13.
+//  Copyright © 2017年 智捷电商APPLE01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello world!")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
